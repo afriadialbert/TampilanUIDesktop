3ff736d3827531cecdd6bf7a3ea79ceb7a2bfe0e
afriadialbert <32488167+afriadialbert@users.noreply.github.com>
2017-12-17 01:03:20 +0700

    Add files via upload

---
+++ Tugas5_PBO12_10116514_AlbertAfriadiSigiro/src/edu/AlbertAfriadi/Tugas5/view/Tugas5_PBO12_10116514_AlbertAfriadiSigiro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.AlbertAfriadi.Tugas5.view;
+
+/**
+ *
+ * @author Acer
+ */
+public class Tugas5_PBO12_10116514_AlbertAfriadiSigiro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
